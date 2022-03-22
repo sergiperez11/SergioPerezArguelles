eccd9678af73dc55371734153282346177a5ed89
sergio <sergiopa147@gmail.com>
2022-03-22 21:36:11 +0100

    primer commit Sergio Pérez Argüelles

---
+++ EjercicioVariables.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad3.actividad3;
+
+/**
+ *
+ * @author ALUMNOS_FP
+ */
+public class EjercicioVariables {
+    public static void main (String [] args){
+        boolean casado = true;
+        int valorMax = 999999;
+        byte diaSem = 1;
+        short diaAnual = 300;
+        char sexo = 'M';
+        long milieg = 1298332800000L;
+        float totalFactura = 10350.678F;
+        long poblacion = 6775235741L;
+String cadena;
+cadena = "El valor de la variable ";
+        
+        System.out.println (cadena + " casado es" + casado);
+        System.out.println (cadena + " valorMax es" + valorMax);
+        System.out.println (cadena + " diaSem es" + diaSem);
+        System.out.println (cadena + " diaAnual es" + diaAnual);
+        System.out.println (cadena + " sexo es" + sexo);
+        System.out.println (cadena + " milieg es" + milieg);
+        System.out.println (cadena + " totalFactura es" + totalFactura);
+        System.out.println (cadena + " población es" + poblacion);
+        
+        System.out.println ("\n"); 
+        
+        System.out.print ("El valor de la variable casado es " + casado + "\n");
+        System.out.print ("El valor de la variable valorMax es " + valorMax + "\n");
+        System.out.print ("El valor de la variable diaSem es" + diaSem + "\n");
+        System.out.print ("El valor de la variable diaAnual es" + diaAnual + "\n");
+        System.out.print ("El valor de la variable sexo es" + sexo + "\n");
+        System.out.print ("El valor de la variable milieg es" + milieg + "\n");
+        System.out.print ("El valor de la variable totalFactura es" + totalFactura + "\n");
+        System.out.print ("El valor de la variable población es" + poblacion + "\n");
+          
+    }
+}
